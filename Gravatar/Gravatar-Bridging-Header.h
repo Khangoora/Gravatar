5898539ede3dafec883e14abdf50adf7e869668b
Khangoora <j.khangoora@gmail.com>
2016-06-30 12:28:39 -0400

    first commit

---
+++ Gravatar/Gravatar-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Gravatar-Bridging-Header.h
+//  Gravatar
+//
+//  Created by Jaskirat Khangoora on 6/30/16.
+//  Copyright © 2016 Jaskirat Khangoora. All rights reserved.
+//
+
+#ifndef Gravatar_Bridging_Header_h
+#define Gravatar_Bridging_Header_h
+
+
+#endif /* Gravatar_Bridging_Header_h */
---
+++ Gravatar/GravatarAPI.swift
@@ -0,0 +1,9 @@
+//
+//  GravatarAPI.swift
+//  Gravatar
+//
+//  Created by Jaskirat Khangoora on 6/30/16.
+//  Copyright © 2016 Jaskirat Khangoora. All rights reserved.
+//
+
+import Foundation
